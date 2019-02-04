ce562dafd18668908c77ecc614b5b1ffdf705536
Noah Omdal <nomdal@Noahs-MacBook-Pro-36.local>
2019-02-04 12:03:20 -0700

    resolving hosting issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = './blog/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'), "/Users/nomdal/Desktop/Diversity_Policy_Site/blog/templates/blog"
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 

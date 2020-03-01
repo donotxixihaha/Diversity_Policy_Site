f2cca447b28d181677cfc405c6f19a4534f08a8c
linni <linni@github.ccs.neu.edu>
2020-03-01 15:46:38 -0800

    update search with regex complex

--- website/search.py
+++ website/search.py
@@ -6,6 +6,7 @@ def search(query, filter=None):
 
 
     STMT_FILTER = ''
+
     if filter:
         first_flag = True
         for i in filter:
@@ -23,6 +24,127 @@ def search(query, filter=None):
                 STMT_FILTER += "school = \'" + i + "\'"
         STMT_FILTER += " AND "
  
+    SPLITTER = ("\"", "-", "[", "]")
+
+    seq_term = []
+    exact_term = set()
+    exclude_term = set()
+    
+    # exact phrase search, # quotes should be even,
+    # otherwise take it as inclusive pattern
+    if SPLITTER[0] in query and query.count(SPLITTER[0]) % 2 == 0:
+        exact_flag = False
+        start_idx = -1
+        end_idx = -1
+
+        i = 0
+        while i < len(query):
+            if not exact_flag and query[i] == SPLITTER[0]:
+                start_idx = i
+                exact_flag = True   
+            elif exact_flag and query[i] == SPLITTER[0]:
+                end_idx = i
+                exact_term.add(query[start_idx+1:end_idx])
+                exact_flag = False
+                query = query[:start_idx] + " " + query[end_idx+1:]
+                i = start_idx - 1
+            i += 1
+
+    query = query.strip(" ") + " "
+    # print(query)
+
+    # exlusive word search
+    if SPLITTER[1] in query:
+        exclude_flag = False
+        start_idx = -1
+        end_idx = -1
+
+        i = 0
+        while i < len(query):
+            if not exclude_flag and query[i] == SPLITTER[1]:
+                start_idx = i
+                exclude_flag = True        
+            elif exclude_flag and query[i] == ' ':
+                end_idx = i
+                exclude_term.add(query[start_idx+1:end_idx])
+                exclude_flag = False
+                query = query[:start_idx] + " " + query[end_idx+1:]
+                i = start_idx - 1
+            i += 1
+
+    query = query.strip(" ")
+    # print(query)
+
+    # a sequence of words search, square brackets should be paired,
+    # otherwise take it as inclusive pattern
+    if SPLITTER[2] in query and SPLITTER[3] in query:
+        seq_flag = False
+        start_idx = -1
+        end_idx = -1
+
+        i = 0
+        while i < len(query):
+            if query[i] == SPLITTER[2]:
+                start_idx = i
+                seq_flag = True
+            elif query[i] == SPLITTER[3]:
+                end_idx = i
+                seq_term.extend(query[start_idx+1:end_idx].split(" "))
+                seq_flag = False
+                query = query[:start_idx] + " " + query[end_idx+1:]
+                i = start_idx - 1
+            i += 1
+    
+    include_term = set(query.strip(" ").split(" "))
+
+    print("seq    :", seq_term)
+    print("exact  :", list(exact_term))
+    print("include:", list(include_term))
+    print("exclude:", list(exclude_term))
+ 
+
+    STMT_TERM = ""
+
+    # abstract LIKE '%seq_term[0]%seq_term[1]%seq_term[2]%...%seq_term[end]%'
+    if seq_term:
+        seq_pattern = "abstract LIKE \'%"
+        for i in seq_term:
+            seq_pattern += "" + i + "%"
+        seq_pattern += "\'"
+        STMT_TERM += "(" + seq_pattern + ")"
+
+    # abstract LIKE '%exact_term[0]%' OR 
+    # abstract LIKE '%exact_term[1]%' OR ...  OR 
+    # abstract LIKE '%exact_term[end]%'
+    exact_term = exact_term.union(include_term)
+    if exact_term:
+        exact_pattern = "abstract LIKE \'%"
+        for i in exact_term:
+            exact_pattern += i + "%\' OR abstract LIKE \'%"
+        exact_pattern = exact_pattern.rstrip("\' OR abstract LIKE \'%")
+        exact_pattern += "\'"
+        if seq_term:
+            STMT_TERM += " AND "
+        STMT_TERM += "(" + exact_pattern + ")"
+
+    # abstract NOT LIKE '%exclude_term[0]%' AND 
+    # abstract NOT LIKE '%exclude_term[1]%' AND ...  AND 
+    # abstract NOT LIKE '%exclude_term[end]%'
+    if exclude_term:
+        print("hhhh", exclude_term)
+        exclude_pattern = "abstract NOT LIKE \'%"
+        for i in exclude_term:
+            exclude_pattern += i + "%\' AND abstract NOT LIKE \'%"
+        print("before:", exclude_pattern)
+        exclude_pattern = exclude_pattern.rstrip(" abstract NOT LIKE \'%")
+        exclude_pattern = exclude_pattern.rstrip("\' AND")
+        print("after :", exclude_pattern)
+        exclude_pattern += "\'"
+        if exact_term:
+            STMT_TERM += " AND "
+        STMT_TERM += "(" + exclude_pattern + ")"
+  
+    
 
     STMT = "SELECT title, school, department, administrator, author, " + \
                   "state, city, latitude, longitude, link, " + \
@@ -31,11 +153,9 @@ def search(query, filter=None):
                    "END) AS published_date, " + \
                   "tags, abstract, text " + \
            "FROM policies " + \
-           "WHERE " + STMT_FILTER + \
-                  "title LIKE \'%" + query + "%\' OR " + \
-                  "abstract LIKE \'%" + query + "%\';"
+           "WHERE " + STMT_FILTER + STMT_TERM + ";"
 
-    # print(STMT)
+    print(STMT)
     print("START Fetching...")
     result = []
     with connection.cursor() as cursor:
@@ -86,6 +206,6 @@ def search(query, filter=None):
 # is used differently than the search() object is in the function calling search_suggest()
 
 def search_suggest(query):
-    results = policies.query.msearch(query)
-    db.session.remove()
-    return results
+    # results = policies.query.msearch(query)
+    # db.session.remove()
+    return []

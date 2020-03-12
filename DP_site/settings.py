bde5febaebe71f82e4ac0c28deede172fab5c350
Zack <deepforce31@gmail.com>
2020-03-12 16:16:03 -0700

    Revise DEBUG field

--- DP_site/settings.py
+++ DP_site/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'seq5*nc-h#yrb4q-14h^fegrd_bx@v#(l#a^!%h(4s8f05kjrw'
-DEBUG = True
+DEBUG = False
 
 # Experimenting here
 ALLOWED_HOSTS = ["localhost", "https://stormy-stream-43261.herokuapp.com/"]
